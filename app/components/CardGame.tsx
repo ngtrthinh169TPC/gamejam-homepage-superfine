c335db4787a35fe6583ecac8b2f452e6927735ff
ngtrthinh <ng.tr.thinh@gmail.com>
2023-09-22 14:24:31 +0700

    feat: featured game section

--- app/components/CardGame.tsx
+++ app/components/CardGame.tsx
@@ -2,7 +2,7 @@ import type { Game } from "~/frontend_test_homepage_data";
 
 export const CardGame = (game: Game) => {
   return (
-    <div className="h-60 rounded-2xl bg-slate-700">
+    <div className="h-72 rounded-2xl bg-slate-700">
       {game.thumbnailImageUrl ? (
         <img
           src={game.thumbnailImageUrl}
@@ -10,13 +10,19 @@ export const CardGame = (game: Game) => {
           className="h-32 w-full rounded-2xl object-cover object-center"
         />
       ) : (
-        <div className="h-32 w-full rounded-2xl bg-slate-400" />
+        <div className="h-32 w-full rounded-2xl bg-slate-600" />
       )}
-      <div className="relative">
-        <h2>{game.name.toUpperCase()}</h2>
-        <p>#{game.genre}</p>
+      <div className="relative flex flex-col gap-2 px-4 py-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-wider">
+            {game.name.toUpperCase()}
+          </h2>
+          <p className="text-purple-400">#{game.genre}</p>
+        </div>
         <p>{game.description}</p>
-        <p className="absolute right-0 top-0">{game.price}$</p>
+        <div className="absolute right-4 top-3 rounded-full bg-slate-500 px-2 py-1 text-lg font-bold">
+          ${game.price}
+        </div>
       </div>
     </div>
   );
